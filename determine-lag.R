81eb350cb1b05581907af5f706b6ae39381638a1
USER NAME <mark.t.douglas@gmail.com>
2021-02-11 21:04:38 +0000

    saving file

--- determine-lag.R
+++ determine-lag.R
@@ -14,17 +14,37 @@ fit_window <- latest_data %>%
   filter(date >= lubridate::dmy('01-Aug-2020')) %>%
   select(date, newAdmissions, newCasesBySpecimenDate)
 
-0:20 %>%
+lag_plot <- 0:20 %>%
   map(~ fit_window %>% 
-        mutate(newCasesBySpecimenDate = lead(newCasesBySpecimenDate, .x)) %>%
+        mutate(newCasesBySpecimenDate = lead(newCasesBySpecimenDate, 
+                                             .x)) %>%
         na.omit) %>%
   map_dbl(~ lm(newAdmissions ~ newCasesBySpecimenDate - 1, .x) %>%
             summary %>%
             `$`(r.squared)) %>%
   tibble( r_squared = ., lead = 0:20) %>%
-  ggplot(aes(x = lead, y = r_squared)) + geom_point() + ggtitle('Lag between cases and admits')
+  ggplot(aes(x = lead, y = r_squared)) + 
+  geom_point() + 
+  ggtitle('Lag between cases and admits')
 
-fit_window %>%
-  mutate(newCasesBySpecimenDate = lead(newCasesBySpecimenDate,6)) %>%
+fit_plot <- fit_window %>%
+  mutate(newCasesBySpecimenDate = lead(newCasesBySpecimenDate,7)) %>%
+  na.omit %>%
   gather(key,value,-date) %>%
+  left_join(fit_window %>%
+              na.omit %>%
+              select(-date) %>%
+              summarise_all(mean) %>%
+              gather(value = divisor),
+            by = 'key') %>%
+  mutate(value = value / divisor) %>%
+  select(-divisor) %>%
   ggplot(aes(x=date,y=value,colour=key)) + geom_point() + theme(legend.position = 'bottom')
+
+ggsave(lag_plot,
+       filename = 'lag_plot.png',
+       width = 8, height = 6, dpi = 100)
+
+ggsave(fit_plot,
+       filename = 'fit_plot.png',
+       width = 8, height = 6, dpi = 100)
